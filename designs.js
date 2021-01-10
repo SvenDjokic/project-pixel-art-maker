da4a17e8da0768508910fae2f82cb9abdf218aa2
SvenDjokic <sven.djokic9@gmail.com>
2021-01-10 21:10:42 +0100

    Feat: store values of sizePicker form in variables

--- designs.js
+++ designs.js
@@ -1,7 +1,16 @@
 // Select color input
 // Select size input
+var height, width, color;
 
 // When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener('submit', function(event) {
+  event.preventDefault();
+  height = document.getElementById('inputHeight').value;
+  width = document.getElementById('inputWidth').value;
+  console.log("Height: " + height + " & Width: " + width); // to check if values are stored correctly
+  }
+)
+
 
 function makeGrid() {
 
