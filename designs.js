8f8f4b40b426872d1cf1dee81d3fbec5f7e27e0a
SvenDjokic <sven.djokic9@gmail.com>
2021-01-12 17:46:41 +0100

    feat: color cell of table when clicking on cell

--- designs.js
+++ designs.js
@@ -14,11 +14,15 @@ sizePicker.addEventListener('submit', function(event) {
 
 function makeGrid(height, width) {
   const table = document.getElementById('pixelCanvas');
+  color = document.getElementById('colorPicker');
   table.innerHTML = '';
   for (var i = 0; i < height; i++ ) {
     let row = table.insertRow(i);
     for (var j = 0; j < width; j++) {
       let cell = row.insertCell(j);
+      cell.addEventListener('click', function(event) {
+        cell.style.backgroundColor = color.value;
+      });
     }
   }
 }
